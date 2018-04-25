3b3c91e29d478a92f16a409f75f3d3638c404007
James Truitt <tru@rockyagirl.com>
2018-04-25 14:58:40 -0600

    starting movies app creation, vue app setup complete

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id='app'>
+    <img src='./assets/logo.png'>
+    <h1> {{ hello }} </h1>
+    <HelloWorld msg='Welcome to Your Vue.js App'/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/HelloWorld.vue';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      hello: 'HelloWorld'
+    }
+  },
   components: {
     HelloWorld
   }
-}
+};
 </script>
 
 <style>
+h1 {
+  color: red;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
