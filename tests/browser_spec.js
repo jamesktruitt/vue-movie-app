516d020cc88110ca8636f97a9e0e7854cd494ef7
James Truitt <tru@rockyagirl.com>
2018-05-29 12:03:46 -0600

    added cypress testing, fixed about page, all tests are passing

--- tests/browser_spec.js
+++ tests/browser_spec.js
@@ -1,27 +1,8 @@
-describe('PHP Travels', () => {
+describe('TRUFLIX', () => {
   it('shows correct web content', () => {
-    cy.visit('/app');
-    cy.get('header img').should('have.attr', 'src', './images/phptravels-logo.png');
-    cy.get('h2').should('have.text', 'Application Test Drive.');
-
-    cy.get('body > header nav > ul > li').as('listItems');
-    cy
-      .get('@listItems')
-      .eq(0)
-      .find('a')
-      .should('have.text', 'Demo');
-    cy
-      .get('@listItems')
-      .eq(1)
-      .find('a')
-      .should('have.text', 'Order');
-    cy
-      .get('ul')
-      .eq(1)
-      .find('li')
-      .eq(0)
-      .should('have.text', 'Product');
-
-    cy.get('nav > button').click();
+    cy.visit('/');
+    cy.get('header img').should('have.attr', 'src', '/img/truflix.98c68506.png');
+    cy.visit('/about')
+    cy.get('h1').should('have.text', 'Brochure Page');
   });
 });
